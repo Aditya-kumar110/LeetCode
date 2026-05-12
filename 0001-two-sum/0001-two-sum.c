a597e66626678d2e7f1f28c1e024c7d9ca48201d
Aditya Kumar <adityakumar2348921@gmail.com>
2026-05-12 15:59:08 +0530

    Time: 111 ms (10.03%), Space: 9.3 MB (72.37%) - LeetHub

--- 0001-two-sum/0001-two-sum.c
+++ 0001-two-sum/0001-two-sum.c
@@ -8,6 +8,7 @@ int* twoSum(int* nums, int numsSize, int target, int* returnSize) {
             if(nums[i]+nums[j]==target){
                arr[0] = i;
                arr[1] = j;
+               break;
             }
         }
     }
