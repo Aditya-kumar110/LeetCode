bf41fda39a48ee82aee3a920c972fcbe5d3e7f44
Aditya Kumar <adityakumar2348921@gmail.com>
2026-05-15 12:04:55 +0530

    Time: 0 ms (100%), Space: 10.5 MB (99.47%) - LeetHub

---
+++ 1572-matrix-diagonal-sum/1572-matrix-diagonal-sum.c
@@ -0,0 +1,11 @@
+int diagonalSum(int** mat, int matSize, int* matColSize) {
+    int sum =0;
+    for(int i = 0;i<matSize;i++){
+        for(int j = 0;j<matColSize[i];j++){
+            if((i==j)|| (i+j)==matSize-1){
+                sum += mat[i][j];
+            }
+        }
+    }
+    return sum;
+}
