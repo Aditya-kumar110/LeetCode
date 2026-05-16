7aa0ec540d29b3c392551c3aa02896502a2b206f
Aditya Kumar <adityakumar2348921@gmail.com>
2026-05-16 10:47:10 +0530

    Time: 0 ms (100%), Space: 9.1 MB (49.88%) - LeetHub

---
+++ 1295-find-numbers-with-even-number-of-digits/1295-find-numbers-with-even-number-of-digits.c
@@ -0,0 +1,13 @@
+int findNumbers(int* nums, int numsSize) {
+    int count = 0;
+    for(int i = 0;i<numsSize;i++){
+        int digit = 0;
+        while(nums[i]>0){
+            nums[i] = nums[i]/10;
+            digit++;
+        }
+        if(digit % 2 == 0)
+            count++;
+    }
+    return count;
+}
