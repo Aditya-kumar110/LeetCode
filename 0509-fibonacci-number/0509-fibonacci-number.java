8327d7f534dbc4ca323fb523413f166df826601b
Aditya Kumar <adityakumar2348921@gmail.com>
2026-09-23 20:38:22 +0530

    Time: 0 ms (100%), Space: 42.3 MB (5.18%) - LeetHub

---
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -0,0 +1,26 @@
+// The Fibonacci numbers, commonly denoted F(n) form a sequence, called the Fibonacci sequence, such that each number is the sum of the two preceding ones, starting from 0 and 1. That is,
+
+// F(0) = 0, F(1) = 1
+// F(n) = F(n - 1) + F(n - 2), for n > 1.
+
+class Solution {
+    public int fib(int n) {
+        if(n == 0)
+            return 0;
+        else if(n==1)
+            return 1;
+        else {
+            int fib1 = 0;
+            int fib2 = 1;
+            int next = 0;
+            int i = 2;
+            while(i<=n){
+                 next = fib1 + fib2;
+                fib1 = fib2;
+                fib2 = next;
+                i++;
+            }
+            return next;
+        }
+    }
+}
